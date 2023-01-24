9fec9ca416f489c074f91dedf3ca141efb7f1c90
Erick Lozano <jesselozano395@gmail.com>
2023-01-23 22:17:07 -0600

    dia 2 de python

---
+++ tema13_a_primero.py
@@ -0,0 +1,8 @@
+
+import tema13_my_module
+
+print("Hola desde primero!")
+tema13_my_module.tem()
+print("!Adios desde primero.py!")
+
+tema13_my_module.main()
---
+++ tema13_my_module.py
@@ -0,0 +1,14 @@
+
+def tem():
+    print("Hola desde my_module.py")
+
+def tem2():
+    print("adios desde my_module.py!")
+
+def main():
+    print("Hola desde main")
+    tem()
+    tem2()
+
+if __name__=='__main__':
+    main()
